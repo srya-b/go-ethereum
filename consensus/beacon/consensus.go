a11bc35e99b4a301fe28a1f057553421043ed45e
Surya Bakshi <surya.bakshi@protonmail.com>
2025-05-11 00:02:07 +0000

    things compile but needs a lot of work

--- consensus/beacon/consensus.go
+++ consensus/beacon/consensus.go
@@ -381,36 +381,36 @@ func (beacon *Beacon) FinalizeAndAssemble(chain consensus.ChainHeaderReader, hea
 
 	// Create the block witness and attach to block.
 	// This step needs to happen as late as possible to catch all access events.
-	if chain.Config().IsVerkle(header.Number, header.Time) {
-		keys := state.AccessEvents().Keys()
+	//if chain.Config().IsVerkle(header.Number, header.Time) {
+	//	keys := state.AccessEvents().Keys()
 
-		// Open the pre-tree to prove the pre-state against
-		parent := chain.GetHeaderByNumber(header.Number.Uint64() - 1)
-		if parent == nil {
-			return nil, fmt.Errorf("nil parent header for block %d", header.Number)
-		}
-		preTrie, err := state.Database().OpenTrie(parent.Root)
-		if err != nil {
-			return nil, fmt.Errorf("error opening pre-state tree root: %w", err)
-		}
-		vktPreTrie, okpre := preTrie.(*trie.VerkleTrie)
-		vktPostTrie, okpost := state.GetTrie().(*trie.VerkleTrie)
+	//	// Open the pre-tree to prove the pre-state against
+	//	parent := chain.GetHeaderByNumber(header.Number.Uint64() - 1)
+	//	if parent == nil {
+	//		return nil, fmt.Errorf("nil parent header for block %d", header.Number)
+	//	}
+	//	preTrie, err := state.Database().OpenTrie(parent.Root)
+	//	if err != nil {
+	//		return nil, fmt.Errorf("error opening pre-state tree root: %w", err)
+	//	}
+	//	vktPreTrie, okpre := preTrie.(*trie.VerkleTrie)
+	//	vktPostTrie, okpost := state.GetTrie().(*trie.VerkleTrie)
 
-		// The witness is only attached iff both parent and current block are
-		// using verkle tree.
-		if okpre && okpost {
-			if len(keys) > 0 {
-				verkleProof, stateDiff, err := vktPreTrie.Proof(vktPostTrie, keys)
-				if err != nil {
-					return nil, fmt.Errorf("error generating verkle proof for block %d: %w", header.Number, err)
-				}
-				block = block.WithWitness(&types.ExecutionWitness{
-					StateDiff:   stateDiff,
-					VerkleProof: verkleProof,
-				})
-			}
-		}
-	}
+	//	// The witness is only attached iff both parent and current block are
+	//	// using verkle tree.
+	//	if okpre && okpost {
+	//		if len(keys) > 0 {
+	//			verkleProof, stateDiff, err := vktPreTrie.Proof(vktPostTrie, keys)
+	//			if err != nil {
+	//				return nil, fmt.Errorf("error generating verkle proof for block %d: %w", header.Number, err)
+	//			}
+	//			block = block.WithWitness(&types.ExecutionWitness{
+	//				StateDiff:   stateDiff,
+	//				VerkleProof: verkleProof,
+	//			})
+	//		}
+	//	}
+	//}
 
 	return block, nil
 }
